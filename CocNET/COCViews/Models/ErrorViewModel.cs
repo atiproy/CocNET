c534f667dba4d224e431a3435af48f30a6d9e471
Atip Roy <313811@cognizant.com>
2021-05-17 12:02:34 +0530

    MVC Project Added

---
+++ CocNET/COCViews/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace COCViews.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
